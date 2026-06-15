5280be4fefbecfc0f442751f3bbab43b594a3163
Jack Verdin <jackverdin@icloud.com>
2026-06-15 14:54:58 -0400

    Inital commit for Project 1

---
+++ PokeDamageCalc.py
@@ -0,0 +1,9 @@
+# Function needed: Type effectiveness calc
+
+# Function needed: Damage Calculator
+
+# Function needed? Type effectiveness to message
+
+# Code needed: User input
+# Code needed: Do the math
+# Code needed: Output message to terminal
