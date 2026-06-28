0fe4511ff8f4f8b04ceadccbe6cd259e0741d326
Jack Verdin <jackverdin@icloud.com>
2026-06-28 13:56:06 -0400

    PokeDamageCalc.py: Updated documentation and whitespace to make code more readable

--- PokeDamageCalc.py
+++ PokeDamageCalc.py
@@ -1,14 +1,23 @@
-"""_summary_
+"""
     PokeDamageCalc.py
     Jack Verdin
     The main file for my Project 1 program. The program acts as a simplified Pokemon damage calculator.
-    6/23/2026
+    6/28/2026
 """
 
 import typechart
 
 # Function: Display a number as a percent of another number, rounded to two decimal places
 def percentof(part=10, total=10):
+    """ Calculates what percent one number is of another.
+
+    Args:
+        part (int, optional): The number to be converted into a percent. Defaults to 10.
+        total (int, optional): The base value. Defaults to 10.
+
+    Returns:
+        float: The percent of part/total, rounded to 2 decimal places
+    """
     if total == 0: return 100
     percent = part / total
     percent *= 100
@@ -17,6 +26,15 @@ def percentof(part=10, total=10):
 
 # Function: While loop input validation in one function
 def valid_input(message, allowed):
+    """Uses a while loop and list to ensure an allowed input is submitted.
+
+    Args:
+        message (string): A message for the input() function
+        allowed (list): A list of allowed inputs
+
+    Returns:
+        string: The input given from the user, but only if it is found in the allowed list
+    """
     given_value = ""
     while len(given_value) < 1:
         given_value = input(message)
@@ -27,6 +45,19 @@ def valid_input(message, allowed):
 
 # Function needed: Damage Calculator
 def damage_calc(basepower=100, total_atk=100, total_def=100, type_mult=1, stab=1, rand_adjust=1):
+    """A simplified version of the Pokemon damage formula.
+
+    Args:
+        basepower (int, optional): The base power of the used move. Defaults to 100.
+        total_atk (int, optional): The effective attack stat, including changes due to stages. Defaults to 100.
+        total_def (int, optional): The effective defence stat, including changes due to stages. Defaults to 100.
+        type_mult (int, optional): The total multiplier due to type matchups. Defaults to 1.
+        stab (int, optional): An additional multiplier for STAB. Defaults to 1.
+        rand_adjust (int, optional): A manual adjustment used in the function. In the real formula is a random number from 0.85 to 1. Defaults to 1.
+
+    Returns:
+        int: The final amount of damage dealt.
+    """
     if type_mult == 0: return 0 # If the type mult is 0, don't bother calculating the damage
     if basepower < 1: basepower = 1 # 1 Is the minimum for power
     if total_atk < 1: total_atk = 1 # 1 Is the minimum for attack
@@ -37,7 +68,15 @@ def damage_calc(basepower=100, total_atk=100, total_def=100, type_mult=1, stab=1
     return final_damage
 
 # Function needed? Type effectiveness to message
-def type_message(type_mult):
+def type_message(type_mult=1):
+    """_summary_
+
+    Args:
+        type_mult (float/int): The input type effectiveness. Defaults to 1.
+
+    Returns:
+        string: The matching message for the type effectiveness.
+    """
     match type_mult:
         case 0:
             return "It had no effect!"
@@ -100,16 +139,20 @@ given_hp = int(input("Please input the defending Pokemon's max HP: "))
 if given_hp < 1: given_hp = 1
 given_power = int(input("Please input the base power of the move used: "))
 if given_power < 1: given_power = 1
+
+
 given_typeatk = valid_input("Please input the type of the move used: ", typechart.type_list)
 given_typeatk = typechart.type_list.index(given_typeatk)
 given_typedef1 = valid_input("Please input the first type of the defending Pokemon: ", typechart.type_list)
 given_typedef1 = typechart.type_list.index(given_typedef1)
 given_typedef2 = valid_input("Please input the second type of the defending Pokemon: ", typechart.type_list)
 given_typedef2 = typechart.type_list.index(given_typedef2)
+total_typemult = typechart.type_calc(given_typeatk, given_typedef1) * typechart.type_calc(given_typeatk, given_typedef2)
+
 given_stab = valid_input("Does the move used benefit from the Same Type Attack Bonus?(Y/N) ", ["Y", "N"])
 if given_stab == "Y": given_stab = 1.5
 else: given_stab = 1
-total_typemult = typechart.type_calc(given_typeatk, given_typedef1) * typechart.type_calc(given_typeatk, given_typedef2)
+
 # Code needed: Do the math
 total_atk = stage_mult(given_atk, given_atkst)
 print(f"With an Attack stat of {given_atk} at stage {given_atkst},\nthe attacking Pokemon has an effective Attack stat of {total_atk}")
